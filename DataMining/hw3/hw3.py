a97781bd9fbe897a7691addc10e161bfc17d0961
Valle <cvalle@zagmail.gonzaga.edu>
2016-10-03 15:46:04 -0700

    Added hw3.py.

---
+++ DataMining/hw3/hw3.py
@@ -0,0 +1,36 @@
+"""
+Andrew Burke, Katreina Carpenter, Cody Valle
+HW3
+"""
+
+"""
+Creates a classifier that predicts mpg values
+using least squares linear regression based on 
+vehicle weight.
+"""
+def step1():
+
+
+"""
+Creates a nearest neighbor classifier for mpg that
+uses the number of cylinders, weight, acceleration
+attributes to predict mpg for k=5.
+"""
+def step2():
+
+
+"""
+Computes the multi-class predictive accuracy and 
+error rate of the two classifiers using separate
+training and test sets.
+"""
+def step3():
+
+
+"""
+Creates confusion matrices for each classifier based
+on the stratified 10-fold cross validation results.
+"""
+def step4():
+
+def main():
