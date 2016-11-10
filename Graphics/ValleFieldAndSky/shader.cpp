e6cad58972bb70a242cd2a2d0888d7dd670fafd9
Valle <codyvalle90@gmail.com>
2016-11-10 14:21:12 -0800

    Graphics daily.

---
+++ Graphics/ValleFieldAndSky/shader.cpp
@@ -0,0 +1,51 @@
+#include <cstdlib>
+#include <iostream>
+#include <fstream>
+
+#  include <GL/glew.h>
+#  include <GL/freeglut.h>
+#ifdef __APPLE__
+#  include <OpenGL/glext.h>
+#else
+#  include <GL/glext.h>
+#endif
+
+using namespace std;
+
+// Function to read text file.
+char* readTextFile(const char* aTextFile)
+{
+   FILE* filePointer = fopen(aTextFile, "rb");
+   char* content = NULL;
+   long numVal = 0;
+
+   fseek(filePointer, 0L, SEEK_END);
+   numVal = ftell(filePointer);
+   fseek(filePointer, 0L, SEEK_SET);
+   content = (char*) malloc((numVal+1) * sizeof(char));
+   fread(content, 1, numVal, filePointer);
+   content[numVal] = '\0';
+   fclose(filePointer);
+   return content;
+}
+
+// Function to initialize shaders.
+int setShader(const char* shaderType, const char* shaderFile)
+{
+   int shaderId;
+   char* shader = readTextFile(shaderFile);
+
+   if (strcmp(shaderType, "vertex") == 0) shaderId = glCreateShader(GL_VERTEX_SHADER);
+   if (strcmp(shaderType, "tessControl") == 0) shaderId = glCreateShader(GL_TESS_CONTROL_SHADER);
+   if (strcmp(shaderType, "tessEvaluation") == 0) shaderId = glCreateShader(GL_TESS_EVALUATION_SHADER);
+   if (strcmp(shaderType, "geometry") == 0) shaderId = glCreateShader(GL_GEOMETRY_SHADER);
+   if (strcmp(shaderType, "fragment") == 0) shaderId = glCreateShader(GL_FRAGMENT_SHADER);
+
+   glShaderSource(shaderId, 1, (const char**) &shader, NULL);
+   glCompileShader(shaderId);
+
+   free(shader);
+
+   return shaderId;
+}
+
